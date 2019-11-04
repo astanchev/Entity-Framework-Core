215d6ffcfff0b6a6c5de1a9c0c5a9dcc1dfc5dc7
Атанас Станчев <mr_Stanchev@hotmail.com>
2019-11-04 11:59:19 +0200

    Add EF Exercise 5

---
+++ EF_Exercise_5/P03_FootballBetting/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace P03_FootballBetting.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = @"Server=.\SQLEXPRESS;Database=Hospital;Integrated Security=True;";
+    }
+}
