510c857a1ecc994201c85c3a183af21c52626468
Атанас Станчев <mr_Stanchev@hotmail.com>
2019-12-01 21:36:03 +0200

    Add exam from 07.04.2019

---
+++ EXAMS/Exam_07.04.2019/Cinema/DataProcessor/ImportDto/HallDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Cinema.DataProcessor.ImportDto
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class HallDto
+    {
+        [Required]
+        [MaxLength(20), MinLength(3)]
+        public string Name { get; set; }
+        public bool Is4Dx { get; set; }
+        public bool Is3D { get; set; }
+        public int Seats { get; set; }
+    }
+}
